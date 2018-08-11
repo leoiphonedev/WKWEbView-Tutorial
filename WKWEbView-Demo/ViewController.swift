8112d345ab48a4e8138969d07f61b660774ee2c1
leoiphonedev <aggarwalaman1985@gmail.com>
2018-08-11 14:32:08 +0530

    Pushing all the code used for tutorial

--- WKWEbView-Demo/ViewController.swift
+++ WKWEbView-Demo/ViewController.swift
@@ -7,14 +7,33 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let urlString = "http://www.apple.com"
+        let request = URLRequest(url: URL(string: urlString)!)
+        self.webView.load(request)
+        
+        self.webView.addObserver(self, forKeyPath: #keyPath(WKWebView.isLoading), options: .new, context: nil)
     }
 
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "loading" {
+            if webView.isLoading {
+                activityIndicator.startAnimating()
+                activityIndicator.isHidden = false
+            } else {
+                activityIndicator.stopAnimating()
+            }
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
